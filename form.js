71f3fd1f933564bd63e92a7c6a5f2850b824ad30
Vincent Tam <vincenttam3000@yahoo.com>
2016-02-12 12:35:35 -0600

    added a function to the submit button to show an alert thanking the user for signing up

--- form.js
+++ form.js
@@ -1,1 +1,7 @@
 var email = document.getElementById('email').required=true;
+
+var button = document.getElementById('submit');
+
+button.addEventListener('click', function(){
+	alert("Thank you for signing up!");
+	});
